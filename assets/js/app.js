54848f7c48fa49031a6da7abd9e9bfcf49ba5ef6
Tadeusz <tadeusz.m.lewandowski@gmail.com>
2017-01-07 12:48:44 +0100

    Add background to canvas, change app object to simulation object

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,56 @@
 "use strict";
 
-function App(canvasId, canvasWidth, canvasHeight){
+
+
+
+
+// Simulation object - canvas
+
+function Simulation(canvasId, canvasWidth, canvasHeight){
 	this.canvasId = canvasId;
 	this.canvasWidth = canvasWidth;
 	this.canvasHeight = canvasHeight;
+	this.shelves = [];
 }
 
-App.prototype = {
+Simulation.prototype = {
 	init: function(){
 		this.canvasObject = document.getElementById(this.canvasId);
 		this.canvasObject.width = this.canvasWidth;
-		this.canvasObject.height = this.canvasHeight;
+		this.canvasObject.height = this.canvasHeight;		
+	},
+	setShelves: function(shelvesArray){
+		this.shelves = shelvesArray;
 	}
 }
 
-const app = new App("simulation", 400, 300);
-app.init();
+const c = new Simulation("simulation", 1080, 720);
+c.init();
+c.setShelves([
+	{
+		name: "dwq"
+	},
+	{
+		name: "dvvvv"
+	}
+]);
+
+
+
+
+// function App(canvasId, canvasWidth, canvasHeight){
+// 	this.canvasId = canvasId;
+// 	this.canvasWidth = canvasWidth;
+// 	this.canvasHeight = canvasHeight;
+// }
+
+// App.prototype = {
+// 	init: function(){
+// 		this.canvasObject = document.getElementById(this.canvasId);
+// 		this.canvasObject.width = this.canvasWidth;
+// 		this.canvasObject.height = this.canvasHeight;
+// 	}
+// }
+
+// const app = new App("simulation", 400, 300);
+// app.init();
